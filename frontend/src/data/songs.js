057f76a867393d693e60da65eb97fd20e8f65f26
FranciscoRV <nosoyunmarinero@gmail.com>
2025-12-09 14:59:41 -0600

    Se agrego favicon

--- frontend/src/data/songs.js
+++ frontend/src/data/songs.js
@@ -2,11 +2,13 @@
 import song1 from "../audio/song1.mp3";
 import song2 from "../audio/song2.mp3";
 import song3 from "../audio/song3.mp3";
+import song4 from "../audio/song4.mp3";
 
 // thumbs
 import thumb1 from "../images/thumbs/thumb1.jpg";
 import thumb2 from "../images/thumbs/thumb2.jpg";
 import thumb3 from "../images/thumbs/thumb3.jpg";
+import thumb4 from "../images/thumbs/thumb4.jpg";
 
 const songs = [
   {
@@ -29,6 +31,13 @@ const songs = [
     file: song3,
     thumbnail: thumb3,
     link: "https://youtu.be/Cl87IeQdwU8?si=6SbxcvPiJ_n2J8hS"
+  },
+  {
+    name: "LOVE & JOY",
+    artist: "Yuki Kimura",
+    file: song4,
+    thumbnail: thumb4,
+    link: "https://youtu.be/89ANKCx94RQ?si=BBUXiGAKt5qir0oZ"
   },
 ];
 
